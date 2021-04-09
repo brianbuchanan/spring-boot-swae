481144da0ae9b4268dfa0995f50e705e2321ec23
Brian Buchanan <bbuchanan@ForwardSlope.local>
2021-04-09 10:16:15 -0700

    Added example path and query parameter operations. Added basic readme with Swagger UI link

--- src/main/java/com/fsi/edu/springboot/swae/web/WebService.java
+++ src/main/java/com/fsi/edu/springboot/swae/web/WebService.java
@@ -1,5 +1,6 @@
 package com.fsi.edu.springboot.swae.web;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.core.MediaType;
@@ -7,13 +8,18 @@ import javax.ws.rs.core.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fsi.edu.springboot.swae.dal.Stock;
 import com.fsi.edu.springboot.swae.dal.StockService;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 /**
@@ -25,6 +31,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 @CrossOrigin
 @RestController
 @RequestMapping
+@OpenAPIDefinition(info = @Info(title = "Stock Web Service"))
 public class WebService {
 
 	@Autowired
@@ -35,12 +42,25 @@ public class WebService {
 	 * 
 	 * @return "Stopped"
 	 */
-	@Operation(summary = "", responses = {
-			@ApiResponse(responseCode = "200", description = ""),
-			@ApiResponse(responseCode = "500", description = "") })
-	@GetMapping(value = "/query", produces = MediaType.APPLICATION_JSON)
-	public List<Stock> query() {
-		return stockService.getAllStocks();
+	@Operation(summary = "Query Stocks", responses = {
+			@ApiResponse(responseCode = "200", description = "OK"),
+			@ApiResponse(responseCode = "500", description = "Error occurred while getting stocks") })
+	@GetMapping(value = "/stocks", produces = MediaType.APPLICATION_JSON)
+	public List<Stock> getStocks(@Parameter(description = "Symbol") @RequestParam(required = false) String symbol) {
+		List<Stock> stocks = new ArrayList<>();
+		if (symbol == null) {
+			stocks.addAll(stockService.getAllStocks());
+		} else {
+			stocks.add(stockService.findStockBySymbol(symbol));
+		}
+		return stocks;
 	}
 	
+	@Operation(summary = "Get Single Stock by Symbol", responses = {
+			@ApiResponse(responseCode = "200", description = "OK"),
+			@ApiResponse(responseCode = "500", description = "Error occurred while getting stock") })
+	@GetMapping(value = "/stocks/{symbol}", produces = MediaType.APPLICATION_JSON)
+	public Stock queryBySymbolQueryParameter(@Parameter(description = "Symbol") @PathVariable String symbol) {
+		return stockService.findStockBySymbol(symbol);
+	}
 }
